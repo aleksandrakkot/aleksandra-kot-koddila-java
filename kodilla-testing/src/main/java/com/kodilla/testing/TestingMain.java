2dbe751836e6d25ae310c5e8eb6c5f71b067aefc
Aleksandra Kot <aleksandrak.kot@gmail.com>
2024-04-14 22:26:21 +0200

    6.2. Pierwszy test jednostkowy - zadanie z kalkulatorem

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        // Testowanie metody add
+        int resultAdd = calculator.add(5, 3);
+        if (resultAdd == 8) {
+            System.out.println("Test dodawania OK");
+        } else {
+            System.out.println("Błąd testu dodawania");
+        }
+
+        // Testowanie metody subtract
+        int resultSubtract = calculator.subtract(10, 3);
+        if (resultSubtract == 7) {
+            System.out.println("Test odejmowania OK");
+        } else {
+            System.out.println("Błąd testu odejmowania");
+        }
     }
 }
