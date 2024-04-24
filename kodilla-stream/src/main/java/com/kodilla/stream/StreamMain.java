2484a3f5231c1aa70de3f6a8d78449823209d279
Aleksandra Kot <aleksandrak.kot@gmail.com>
2024-04-24 20:48:10 +0200

    7.2 niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 import com.kodilla.stream.beautifier.PoemBeautifier;
@@ -29,6 +30,9 @@ public class StreamMain {
         // Custom lambda expressions
         poemBeautifier.beautify("Sample text", text -> "*** " + text + " ***");
         poemBeautifier.beautify("Sample text", text -> text.replace(" ", "-"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
